a55ba69d2d57e0bff0559d9653584ab2cb54c752
Adrian Engel <adihimself@gmail.com>
2013-02-08 16:38:44 +0100

    Update jquery.tiny-acc.js
    collapsible effect added

--- jquery.tiny-acc.js
+++ jquery.tiny-acc.js
@@ -1,6 +1,11 @@
 $(document).ready(function(e) {
-  $('.acc-header').click(function(){
-		$('.open').removeClass('open').stop().slideToggle();
-		$(this).next('.acc-content').addClass('open').stop().slideToggle();	
+	$('.acc-header').click(function(){
+		if($(this).next('.acc-content').hasClass('open')) {
+			$(this).next('.acc-content').removeClass('open').slideToggle();
+		} else {
+			$('.open').removeClass('open').stop().slideToggle(300);
+			$(this).addClass('openheader');
+			$(this).next('.acc-content').addClass('open').stop().slideToggle(300);
+		}
 	});
 });
